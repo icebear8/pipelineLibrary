f2529f9862138ecce32e6d9b2d3b862e5069ca7c
icebear8 <ebert.dominik@gmail.com>
2017-11-17 21:09:56 +0100

    Try a simple variable

--- src/docker/utils.groovy
+++ src/docker/utils.groovy
@@ -1,12 +1,6 @@
 package docker
 
-def REPO_LATEST_BRANCH = 'master'
-def REPO_STABLE_BRANCH = 'stable'
-def REPO_RELEASE_BRANCH = 'release'
-
-def DOCKER_TAG_LATEST = 'latest'
-def DOCKER_TAG_STABLE = 'stable'
-def DOCKER_NO_TAG_BUILD = 'build'
+def SOME_EXTERNAL_VAR = "External var"
 
 def helloPipelineLibrary() {
   println "Hello from the library"
