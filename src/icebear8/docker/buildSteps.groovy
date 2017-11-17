dba3fb4ba16c24da9ae48f50b06f5683d2e5e117
icebear8 <ebert.dominik@gmail.com>
2017-11-17 22:16:32 +0100

    Extract docker build steps

---
+++ src/icebear8/docker/buildSteps.groovy
@@ -0,0 +1,36 @@
+package icebear8.docker
+
+def createDockerBuildStep(imageId, dockerFilePath, isRebuild) {
+  def buildArgs = "${dockerFilePath}"
+  
+  if (isRebuild == true) {
+    buildArgs = "--no-cache --rm ${dockerFilePath}"
+  }
+
+  return {
+    stage("Build image ${imageId}") {
+      echo "Build image: ${imageId} with dockerfile ${dockerFilePath}"
+      docker.build("${imageId}", "${buildArgs}")
+    }
+  }
+}
+
+def createDockerPushStep(imageId, remoteTag) {
+  return {
+    stage("Push image ${imageId} to ${remoteTag}") {
+      echo "Push image: ${imageId} to remote with tag ${remoteTag}"
+      
+      docker.image("${imageId}").push("${remoteTag}")
+    }
+  }
+}
+
+def createRemoveImageStep(imageId, localImageTag, remoteImageTag) {
+return {
+    stage("Remove image ${imageId}") {
+      echo "Remove image: ${imageId}, tags: ${localImageTag}, ${remoteImageTag}"
+      sh "docker rmi ${imageId}:${localImageTag}"
+      sh "docker rmi ${imageId}:${remoteImageTag}"
+    }
+  }
+}
