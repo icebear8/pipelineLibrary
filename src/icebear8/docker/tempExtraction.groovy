b86b47b9cd8515545d9741ffef03e7f94d27cb6b
icebear8 <ebert.dominik@gmail.com>
2017-11-17 23:59:17 +0100

    Add the missing methods to the temp file

--- src/icebear8/docker/tempExtraction.groovy
+++ src/icebear8/docker/tempExtraction.groovy
@@ -134,3 +134,44 @@ def evaluateReleaseTag(releaseBranch, imageName) {
   
   return releaseBranch.substring(indexOfImage + imageName.length() + 1) // +1 because of additional sign between image id and release tag
 }
+
+def createDummyStage(name, content) {
+  stage("${name}") {
+    echo "${content}"
+  }
+}
+
+def buildImage(imageId, dockerFilePath, isRebuild) {
+  def buildArgs = "${dockerFilePath}"
+  
+  if (isRebuild == true) {
+    buildArgs = "--no-cache --rm ${dockerFilePath}"
+  }
+
+  return {
+    stage("Build image ${imageId}") {
+      echo "Build image: ${imageId} with dockerfile ${dockerFilePath}"
+      docker.build("${imageId}", "${buildArgs}")
+    }
+  }
+}
+
+def pushImage(imageId, remoteTag) {
+  return {
+    stage("Push image ${imageId} to ${remoteTag}") {
+      echo "Push image: ${imageId} to remote with tag ${remoteTag}"
+      
+      docker.image("${imageId}").push("${remoteTag}")
+    }
+  }
+}
+
+def removeImage(imageId, localImageTag, remoteImageTag) {
+  return {
+    stage("Remove image ${imageId}") {
+      echo "Remove image: ${imageId}, tags: ${localImageTag}, ${remoteImageTag}"
+      sh "docker rmi ${imageId}:${localImageTag}"
+      sh "docker rmi ${imageId}:${remoteImageTag}"
+    }
+  }
+}
