c41e4540f884cb5652751790edb5759d772e7711
icebear8 <ebert.dominik@gmail.com>
2017-11-17 21:00:39 +0100

    Add docker and branch tags

--- src/docker/utils.groovy
+++ src/docker/utils.groovy
@@ -1,5 +1,13 @@
 package docker
 
+def REPO_LATEST_BRANCH = 'master'
+def REPO_STABLE_BRANCH = 'stable'
+def REPO_RELEASE_BRANCH = 'release'
+
+def DOCKER_TAG_LATEST = 'latest'
+def DOCKER_TAG_STABLE = 'stable'
+def DOCKER_NO_TAG_BUILD = 'build'
+
 def helloPipelineLibrary() {
   println "Hello from the library"
 }
