672ca114d42470075faa410855306c9243afc362
icebear8 <ebert.dominik@gmail.com>
2018-03-13 15:57:22 +0100

    Call static without class instance

--- vars/projectArctic.groovy
+++ vars/projectArctic.groovy
@@ -1,6 +1,5 @@
 #!/usr/bin/env groovy
 
 def buildMethod(projectSettings) {
-  def buildDefinition = new icebear8.projects.arctic.buildDefinition()
-  buildDefinition.buildMethod(projectSettings)
+  icebear8.projects.arctic.buildDefinition.buildMethod(projectSettings)
 }
