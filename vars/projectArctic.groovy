83b3489050e184fd1a45ad69b70d2b6079bcc062
icebear8 <ebert.dominik@gmail.com>
2018-03-13 16:04:13 +0100

    Try to load the path dynamically

--- vars/projectArctic.groovy
+++ vars/projectArctic.groovy
@@ -2,7 +2,8 @@
 
 def buildMethod(projectSettings) {
   def tempMethod = "buildMethod"
+  def packagePathToExecute = "icebear8.projects.arctic.buildDefinition"
 
-  def buildDefinition = new icebear8.projects.arctic.buildDefinition()
+  def buildDefinition = new "${packagePathToExecute}"()
   buildDefinition."${tempMethod}"(projectSettings)
 }
