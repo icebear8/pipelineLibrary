bc6b80b6dc5a3367fcff8a99900852e7c3df2daa
icebear8 <ebert.dominik@gmail.com>
2018-04-01 08:37:39 +0200

    Change project build script entry point

--- vars/projectArctic.groovy
+++ vars/projectArctic.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
 def buildMethod(projectSettings) {
-  def instructions = new icebear8.projects.arctic.BuildInstructions()
+  def instructions = new icebear8.projects.Arctic()
   instructions.buildMethod(projectSettings)
 }
