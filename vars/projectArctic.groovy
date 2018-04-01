2b05774f625eb12d24ce20bead7de01b5947f255
icebear8 <ebert.dominik@gmail.com>
2018-04-01 08:55:42 +0200

    Try class as string

--- vars/projectArctic.groovy
+++ vars/projectArctic.groovy
@@ -1,6 +1,7 @@
 #!/usr/bin/env groovy
 
 def buildMethod(projectSettings) {
-  def instructions = new icebear8.projects.Arctic()
+  def p = "Arctic"
+  def instructions = new icebear8.projects."${p}"()
   instructions.buildMethod(projectSettings)
 }
