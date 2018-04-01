458a96496ac9e9057cda1ab7788aa465e220049d
icebear8 <ebert.dominik@gmail.com>
2018-04-01 09:03:03 +0200

    Without def

--- vars/projectArctic.groovy
+++ vars/projectArctic.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/env groovy
 
 def buildMethod(projectSettings) {
-  def p = "icebear8.projects.Arctic"
+  p = "icebear8.projects.Arctic"
   def instructions = new '${p}'()
   instructions.buildMethod(projectSettings)
 }
