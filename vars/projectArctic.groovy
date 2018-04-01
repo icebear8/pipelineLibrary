3a762efc9b817c26b49b450859710194012b69d2
icebear8 <ebert.dominik@gmail.com>
2018-04-01 09:02:01 +0200

    Try other string

--- vars/projectArctic.groovy
+++ vars/projectArctic.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/env groovy
 
 def buildMethod(projectSettings) {
-  def p = "icebear8.projects.Arctic()"
-  def instructions = new "${p}"
+  def p = "icebear8.projects.Arctic"
+  def instructions = new '${p}'()
   instructions.buildMethod(projectSettings)
 }
